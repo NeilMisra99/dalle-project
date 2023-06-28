3ead6b460a964676c1298e4fdc6f56da9b4dc172
NeilMisra99 <116110498+NeilMisra99@users.noreply.github.com>
2023-06-28 16:17:06 -0400

    Changed code to include period at end of prompt and handle image names accordingly

--- azure/src/functions/getChatGPTSuggestion.js
+++ azure/src/functions/getChatGPTSuggestion.js
@@ -9,7 +9,7 @@ app.http("getChatGPTSuggestion", {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt:
-        "Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.",
+        "Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes, and end the prompt with a period.",
       max_tokens: 100,
       temperature: 0.8,
     });
